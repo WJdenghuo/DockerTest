f6d400f507679e6e44e45d2ffc7123c623446305
帅 郭 <wanjia_denghuo@sina.com>
2019-08-19 17:20:15 +0800

    docker测试

--- DockerTest/Controllers/ValuesController.cs
+++ DockerTest/Controllers/ValuesController.cs
@@ -6,11 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DockerTest.Controllers
 {
+    /// <summary>
+    /// docker 测试
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        /// <summary>
+        /// GET api/values
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
@@ -18,6 +25,11 @@ namespace DockerTest.Controllers
         }
 
         // GET api/values/5
+        /// <summary>
+        /// GET api/values/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -25,18 +37,31 @@ namespace DockerTest.Controllers
         }
 
         // POST api/values
+        /// <summary>
+        /// POST api/values
+        /// </summary>
+        /// <param name="value"></param>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
+        /// <summary>
+        /// PUT api/values/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
+        /// <summary>
+        /// DELETE api/values/5
+        /// </summary>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
